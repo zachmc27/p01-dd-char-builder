758057745c32ebc9dd8de5c902f19e05d26503ef
zachmc27 <dzdudes1213@gmail.com>
2025-01-17 23:17:29 -0600

    logic file with localstorage func and buttons functional in backstory

---
+++ assets/js/logic.js
@@ -0,0 +1,20 @@
+/* save data to local storage */
+function storeLocalStorage (data) {
+    localStorage.setItem(`${data}`, JSON.stringify(data))
+    }
+
+    /* get data from local storage */
+    function readLocalStorage (data) {
+    return JSON.parse(localStorage.getItem(`${data}`))
+    }
+    /* redirect page */
+    let redirectURL = '';
+    
+    const redirectPage = function (url) {
+      redirectURL = url;
+      location.assign(url);
+    };
+
+
+
+    
